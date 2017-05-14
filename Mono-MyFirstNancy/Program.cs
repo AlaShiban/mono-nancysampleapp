f262263127a9f7da6de3519c0b06cb1debc21eff
Ala Shiban <alashiban@Alas-MacBook-Air.local>
2017-05-14 10:11:35 -0700

    initial commit

---
+++ Mono-MyFirstNancy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace MonoMyFirstNancy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			using (var host = new NancyHost(new Uri("http://127.0.0.1:4321")))
+			{
+				host.Start();
+				Console.WriteLine("Running on http://127.0.0.1:4321");
+				Console.ReadLine();
+			}
+        }
+    }
+
+	public class SampleModule : Nancy.NancyModule
+	{
+		public SampleModule()
+		{
+			Get["/"] = _ => "Hello World!";
+		}
+	}
+}
